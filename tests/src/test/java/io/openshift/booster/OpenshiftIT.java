b5eb0139bd73f2f795842c4ac7c350d2076baef7
Juan Gonzalez <juagonza@redhat.com>
2018-10-05 11:11:58 +0200

    Sync with community and update to 2.2.0.ER5

--- tests/src/test/java/io/openshift/booster/OpenshiftIT.java
+++ tests/src/test/java/io/openshift/booster/OpenshiftIT.java
@@ -42,8 +42,8 @@ import static org.junit.Assert.assertThat;
  */
 @RunWith(Arquillian.class)
 public class OpenshiftIT {
-    private static final String NAME_SERVICE_APP = "wfswarm-cache-cute-name";
-    private static final String GREETING_SERVICE_APP = "wfswarm-cache-greeting";
+    private static final String NAME_SERVICE_APP = "thorntail-cache-cute-name";
+    private static final String GREETING_SERVICE_APP = "thorntail-cache-greeting";
 
     @RouteURL(NAME_SERVICE_APP)
     @AwaitRoute(statusCode = {200, 204}, path = "/health")
